78eea4574df32a908441db2f0d6f43a8a5f38f4c
Snasn3n <jonasneugebauer.01@gmx.de>
2026-01-14 12:17:28 +0100

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/frontend-puppyracer/',  // FEST HARDCODED für Produktion
+  base: process.env.NODE_ENV === 'production' ? '/frontend-puppyracer/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
